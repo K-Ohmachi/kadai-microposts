ed7477f707b34788dd50acd6baa674adc2c3618b
K.Ohmachi <gennsoutyuubou@yahoo.co.jp>
2023-05-16 12:02:25 +0000

    follow,unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,40 +1,86 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
-
-use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
-class UsersController extends Controller
+class UserFollowController extends Controller
 {
-    public function index()
+    /**
+     * ユーザをフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function store($id)
     {
-        // ユーザ一覧をidの降順で取得
-        $users = User::orderBy('id', 'desc')->paginate(10);
+        // 認証済みユーザ（閲覧者）が、 idのユーザをフォローする
+        \Auth::user()->follow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
+    }
 
-        // ユーザ一覧ビューでそれを表示
-        return view('users.index', [
-            'users' => $users,
-        ]);
+    /**
+     * ユーザをアンフォローするアクション。
+     *
+     * @param  $id  相手ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // 認証済みユーザ（閲覧者）が、 idのユーザをアンフォローする
+        \Auth::user()->unfollow($id);
+        // 前のURLへリダイレクトさせる
+        return back();
     }
     
-    public function show($id)
+    /**
+     * ユーザのフォロー一覧ページを表示するアクション。
+     *
+     * @param  $id  ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function followings($id)
     {
         // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
-        
+
         // 関係するモデルの件数をロード
         $user->loadRelationshipCounts();
-        
-        // ユーザーの投稿一覧を作成日時の降順で取得
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
 
-        // ユーザ詳細ビューでそれを表示
-        return view('users.show', [
+        // ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+
+        // フォロー一覧ビューでそれらを表示
+        return view('users.followings', [
             'user' => $user,
-            'microposts' => $microposts,
+            'users' => $followings,
+        ]);
+    }
+
+    /**
+     * ユーザのフォロワー一覧ページを表示するアクション。
+     *
+     * @param  $id  ユーザのid
+     * @return \Illuminate\Http\Response
+     */
+    public function followers($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロワー一覧を取得
+        $followers = $user->followers()->paginate(10);
+
+        // フォロワー一覧ビューでそれらを表示
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
         ]);
     }
 }
